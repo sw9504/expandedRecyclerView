407ef02e738a45266b70a9bb47873a583349f4f7
sw9504 <sladewilson9504@gmail.com>
2022-09-21 20:25:02 -0300

    Initial commit, prepare navgraph

---
+++ app/src/main/java/com/utn/expandedrecyclerview/fragments/expandedFragment.kt
@@ -0,0 +1,21 @@
+package com.utn.expandedrecyclerview.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.utn.expandedrecyclerview.R
+
+class expandedFragment : Fragment() {
+    lateinit var v : View
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        v =  inflater.inflate(R.layout.fragment_expanded, container, false)
+        return v
+    }
+}
